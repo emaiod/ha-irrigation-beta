1f6838b6639ab5468e2212027fafd187eaa23993
emaiod <96419865+emaiod@users.noreply.github.com>
2026-08-20 20:24:28 +0200

    Add irrigation integration constants

---
+++ custom_components/irrigation_controller/const.py
@@ -0,0 +1,6 @@
+DOMAIN = "irrigation_controller"
+STORAGE_KEY = f"{DOMAIN}.data"
+STORAGE_VERSION = 1
+PANEL_URL = "irrigation-controller"
+PANEL_ELEMENT = "irrigation-controller-panel"
+STATIC_URL = "/irrigation_controller_static"
